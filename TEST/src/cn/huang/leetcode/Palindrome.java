db4d4677b8348f7dc0a9a8577b245382536d8656
ThinkHuang <1070014845@qq.com>
2017-06-27 10:34:13 +0800

    添加回文数的算法，不进行额外空间的使用，临时变量可以使用

---
+++ TEST/src/cn/huang/leetcode/Palindrome.java
@@ -0,0 +1,42 @@
+﻿package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * 判断一个整数是不是回文数
+ * 注意，不能使用额外的空间
+ * 此题的一个解法为，一个数的最大后端与一个数的最大前端相等，那么这个数就是回文数
+ * 例如：123321
+ * 最大前端为:12332
+ * 最大后端为:23321
+ * @author dell
+ *
+ */
+public class Palindrome {
+
+	@Test
+	public void test(){
+		int num = 1001;
+		System.out.println(isPalindrome(num));
+	}
+	
+	 public boolean isPalindrome(int x) {
+		if(x < 0) return false;
+		if(x == 0) return true;
+		int y = 0;
+		int z = x;
+		while(z >= 10){
+			y *= 10;
+			y += (z % 10);
+			System.out.println(y);
+			z = z / 10;
+		}
+		return y == x / 10;
+		/**
+		 * 下面的方式由于使用的额外的空间，不经采用
+		 */
+//		String temp = String.valueOf(x);
+//		String source = temp;
+//		return new StringBuffer(temp).reverse().toString().equalsIgnoreCase(source);
+	 }
+}
