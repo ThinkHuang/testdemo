0a7d89823fa1c16c75dde99bac4b07c42d14bf93
ThinkHuang <1070014845@qq.com>
2017-07-04 21:30:41 +0800

    在不开辟新空间的情况下，怎么去除一个有序数字数组中的重复数据，并返回去除重复后的数组长度，非常巧妙的一点在于，存在两个下标x和y，起始时两个下标的位置相同，一个下标记录该数组没有重复数字的下标x，而另一个下标y则遍历整个数组，如果出现了相同，那么x的下标不变，y的下标继续，直到遇到了下标值再次不同后，再将y下标下的数字复制给x下标中，从而完成去重。所以，最后的数据是数组原长度-重复数据的个数的下标中的数字

---
+++ TEST/src/cn/huang/leetcode/RemoveDuplicates.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dell
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * For example,
+ * Given input array nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length. 
+ */
+public class RemoveDuplicates {
+	
+	/**
+	 * 这里有一个巧妙的地方在于，如果是相等的元素的话，那么附近的几个元素都是相同的
+	 */
+	@Test
+	public void test(){
+		int[] nums = {1,2,5,5,5,12,32,45,65};
+		System.out.println(removeDuplicates(nums));
+	}
+	
+	
+	/**
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int removeDuplicates(int[] nums) {
+		int j = 0;
+        for (int i = 0; i < nums.length; i++) 
+    		if(nums[i] != nums[j])
+    			nums[++j] = nums[i];
+		return ++j;
+    }
+}
