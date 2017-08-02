df6e7bf003f51151ae4e5517cfc3de5a40eaa92e
ThinkHuang <1070014845@qq.com>
2017-08-02 11:23:06 +0800

    利用xor机制来实现单数的查找，N ^ N = 0, 0 ^ N = N的机制“

---
+++ TEST/src/cn/huang/leetcode/SingleNumber.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dell
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? 
+ */
+public class SingleNumber {
+	
+	@Test
+	public void test(){
+		int[] nums = {2,8,6,9,8,6,2};
+		System.out.println(singleNumber(nums));
+	}
+	
+	/**
+	 * 使用xor的特性，相同的数据xor后得到的数值就是0
+	 * @param nums
+	 * @return
+	 */
+	public int singleNumber(int[] nums) {
+		int result = 0;
+        for(int i : nums) {
+            result ^= i;
+            System.out.println("result:" + result);
+        }
+        return result;
+    }
+}
