8d7b410e3ec17774a0effcc3f0a0c0e0c55c94c7
ThinkHuang <1070014845@qq.com>
2017-08-09 18:36:51 +0800

    添加将输入的数字转换成Excel文档上的列名称和两个数字相加的变种算法

---
+++ TEST/src/cn/huang/leetcode/ConvertToTitle.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dell
+ *
+ */
+public class ConvertToTitle {
+
+	@Test
+	public void test(){
+		System.out.println(convertToTitle(353));
+	}
+	
+	public String convertToTitle(int n) {
+        final int EN_LENGTH = 26;
+        StringBuilder sb = new StringBuilder();
+        char[] baseChar = {' ','A','B','C','D','E','F','G','H','I',
+        					'J','K','L','M','N','O','P','Q','R',
+        					'S','T','U','V','W','X','Y','Z'
+                           };
+        if(n < EN_LENGTH){
+            return String.valueOf(baseChar[n]);
+        } else {
+        	while(n / EN_LENGTH > 0){
+        		int remaining = n % EN_LENGTH;
+        		sb.append((char)('A' + remaining));
+        		n = n / EN_LENGTH;
+        	}
+        }
+        return sb.toString();
+    }
+	
+	 public String convertToTitle1(int n) {
+	        StringBuilder result = new StringBuilder();
+
+	        while(n>0){
+	            n--;
+	            result.insert(0, (char)('A' + n % 26));
+	            n /= 26;
+	        }
+
+	        return result.toString();
+	    }
+}
---
+++ TEST/src/cn/huang/leetcode/TwoSum.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dell
+ *
+ */
+public class TwoSum {
+	
+	@Test
+	public void test(){
+		int[] numbers = {-1, 0};
+		int target = -1;
+		twoSum(numbers, target);
+	}
+	
+	/**
+	 * first style
+	 * @param numbers
+	 * @param target
+	 * @return
+	 */
+	 public int[] twoSum(int[] numbers, int target) {
+		    int[] result = new int[2];
+	        int length = numbers.length;
+//	        int middle = length / 2;
+//	        if((length - middle < 2) || target > numbers[length-1]){
+//	        	throw new IllegalArgumentException(" No two sum solution");
+//	        }
+	        first:for(int i = 0; i < length; i++){
+	        	int temp = target - numbers[i];
+	        	for (int j = i+1; j < length; j++) {
+	        		if(target > 0 && numbers[j]==0) continue;
+					if(i != j && temp == numbers[j]){
+						result[0] = i+1;
+						result[1] = j+1;
+						break first;
+					}
+				}
+	        }
+//	        if(result[0] <= 0 || result[1] <= 0){
+//	        	throw new IllegalArgumentException(" No two sum solution");
+//	        }
+	        System.out.println("result[0]:" + result[0] + " result[1]:" + result[1]);
+	        return result;
+     }
+	 
+	 /**
+	  * second style
+	  * @param numbers
+	  * @param target
+	  * @return
+	  */
+	 public int[] twoSum1(int[] numbers, int target) {
+		 int[] result = new int[2];
+	        int length = numbers.length;
+	        int middle = length / 2;
+	        if((length - middle < 2) || target > numbers[length-1]){
+	        	throw new IllegalArgumentException(" No two sum solution");
+	        }
+	        int left = 0; int right = length - 1;
+	        while(left < right){
+	        	int v = numbers[left] + numbers[right];
+	        	if(target == v){
+	        		result[0] = left + 1;
+	        		result[1] = right + 1;
+	        		break;
+	        	}else if(target > v){
+	        		left++;
+	        	}else{
+	        		right--;
+	        	}
+	        }
+	        System.out.println("result[0]:" + result[0] + " result[1]:" + result[1]);
+	        return result;
+	 }
+	 
+}
