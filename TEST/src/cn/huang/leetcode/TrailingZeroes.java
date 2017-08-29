21ae62f7f588b53cd67d237243fd104919b41b67
ThinkHuang <1070014845@qq.com>
2017-08-29 21:38:55 +0800

    添加不知道的算法

---
+++ TEST/src/cn/huang/leetcode/TrailingZeroes.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author dell
+ *
+ */
+public class TrailingZeroes {
+	
+	@Test
+	public void test(){
+		System.out.println(trailingZeroes(100));
+	}
+	
+	public int trailingZeroes(int n) {
+		int count = 0;
+		while(n > 1){
+			int temp = n / 5;
+			count += temp;
+			n = temp;
+		}
+		return count;
+    }
+}
