0df4ac76da181f04143630a21f5533f6b98629b6
ThinkHuang <1070014845@qq.com>
2017-08-30 18:33:37 +0800

    update

--- TEST/src/cn/huang/leetcode/Rotate.java
+++ TEST/src/cn/huang/leetcode/Rotate.java
@@ -14,11 +14,32 @@ public class Rotate {
 	@Test
 	public void test(){
 		int[] nums = {1,2,3,4,5,6,7};
-		rotate(nums, 4);
+		rotate1(nums, 4);
 	}
 	
+	/**
+	 * 采用新数组
+	 * @param nums
+	 * @param k
+	 */
 	 public void rotate(int[] nums, int k) {
 	        if(k > nums.length || k < 0)
 	        	throw new RuntimeException("illegal step");
+	        
+	        int[] newNum = new int[nums.length];
+	        int index = 0;
+	        for (int i = nums.length - 1; i >= k; i--) {
+				newNum[index++] = nums[i];
+			}
+	        for (int i = 0; i < k; i++) {
+				newNum[index++] = nums[i];
+			}
+	        for (int i = 0; i < newNum.length; i++) {
+				System.out.println("index " + i + ": " +newNum[i]);
+			}
+	 }
+	 
+	 public void rotate1(int[] nums, int k) {
+		 
 	 }
 }
