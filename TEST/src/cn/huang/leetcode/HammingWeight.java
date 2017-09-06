1ecff3733e586c3b161ff4473b5e18e9452e9d39
ThinkHuang <1070014845@qq.com>
2017-09-06 17:59:14 +0800

    添加比特位的调转算法和计算一个数字的比特位中1的位数算法

---
+++ TEST/src/cn/huang/leetcode/HammingWeight.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author huangyejun
+ * 记住一点：一旦采用无符号二进制表示，每个数字的表示法只有一种且唯一
+ */
+public class HammingWeight {
+
+	@Test
+	public void test(){
+		System.out.println(hammingWeight(Integer.MIN_VALUE));
+	}
+	
+	 public int hammingWeight(int n) {
+	     int num = 0;
+	     Long m = new Long(n);
+	     int count = 32;
+		 while (--count >= 0){
+			 if((m&0x1) == 1)
+				 ++num; 
+			 m = m >>> 1;
+		 }
+		 return num;
+	 }
+}
---
+++ TEST/src/cn/huang/leetcode/ReverseBits.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.huang.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Reverse bits of a given 32 bits unsigned integer.
+ * For example
+ * given input 43261596 (represented in binary as 00000010100101000001111010011100), 
+ * return 964176192 (represented in binary as 00111001011110000010100101000000).
+ * @author dell
+ *
+ */
+public class ReverseBits {
+	
+	@Test
+	public void test(){
+		int n = 43261596;
+		reverseBits(n);
+	}
+	
+	/**
+	 * Integer的自带方法
+	 * @param n
+	 * @return
+	 */
+	public int reverseBits(int n) {
+		return reverse(n);
+    }
+	
+	public static int reverse(int i) {
+		i = (i & 0x55555555) << 1 | (i >>> 1) & 0x55555555;
+		i = (i & 0x33333333) << 2 | (i >>> 2) & 0x33333333;
+		i = (i & 0x0f0f0f0f) << 4 | (i >>> 4) & 0x0f0f0f0f;
+		i = (i << 24) | ((i & 0xff00) << 8) |
+		    ((i >>> 8) & 0xff00) | (i >>> 24);
+	return i;
+    }
+}
