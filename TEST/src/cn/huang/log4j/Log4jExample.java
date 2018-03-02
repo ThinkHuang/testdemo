76732e67dd5e8e06345878eaef3a7c47c6766030
ThinkHuang <1070014845@qq.com>
2018-03-02 18:02:27 +0800

    添加对log4j的测试

---
+++ TEST/src/cn/huang/log4j/Log4jExample.java
@@ -0,0 +1,26 @@
+package cn.huang.log4j;
+
+import java.net.URL;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jExample
+{
+    static Logger log = Logger.getLogger(Log4jExample.class.getName());
+    
+    public static void main(String[] args)
+    {
+        URL url = Log4jExample.class.getClassLoader().getResource(".");
+        String path = url.getPath() + "cn/huang/config/Log4j.properties";
+        PropertyConfigurator.configure(path);   
+        log.setLevel(Level.WARN);
+        
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
+    }
+}
