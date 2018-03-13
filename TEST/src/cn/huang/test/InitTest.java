db43c108307ed6d1b797447a39fa518a56abc511
ThinkHuang <1070014845@qq.com>
2018-03-13 15:03:23 +0800

    添加对类的初始化的测试

--- TEST/src/cn/huang/test/InitTest.java
+++ TEST/src/cn/huang/test/InitTest.java
@@ -1,13 +1,32 @@
 package cn.huang.test;
 
-public class InitTest {
-	public static void main(String[] args) {
-		
-	}
-
-	static {
-		i = 0 ;
-		//System.out.println(i);
-	}
-	static int i = 1;
-}
+public class InitTest {  
+    public static int k = 0;  
+    public static TestVar t1 = new TestVar("t1");  
+    public static TestVar t2 = new TestVar("t2");  
+    public static int i = print("i");  
+    public static int n = 99;  
+    public int j = print("j");  
+    {  
+        print("ππ‘Ï");  
+    }  
+    static {  
+        print("æ≤Ã¨");  
+    }  
+  
+    public TestVar(String str) {  
+        System.out.println((++k) + ":" + str + "    i=" + i + " n=" + n);  
+        ++i;  
+        ++n;  
+    }  
+  
+    public static int print(String str) {  
+        System.out.println((++k) + ":" + str + "    i=" + i + " n=" + n);  
+        ++n;  
+        return ++i;  
+    }  
+  
+    public static void main(String[] args) {  
+        InitTest t = new InitTest ("init");  
+    }  
+}  
