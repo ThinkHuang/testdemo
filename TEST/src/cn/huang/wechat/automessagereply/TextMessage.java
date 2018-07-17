9ada31a20d0dac9d3ba1883bc3ad34b6946aedc7
ThinkHuang <1070014845@qq.com>
2018-07-17 16:54:20 +0800

    添加微信发送被动回复消息，包括文本消息和图片消息，后续需要增加其他5中消息类型

---
+++ TEST/src/cn/huang/wechat/automessagereply/TextMessage.java
@@ -0,0 +1,25 @@
+package cn.huang.wechat.automessagereply;
+
+/**
+ * 文本消息
+ * @author huangyejun
+ *
+ */
+public class TextMessage extends BaseMessage
+{
+    /**
+     * 回复内容
+     */
+    private String Content;
+    
+    public String getContent()
+    {
+        return Content;
+    }
+    
+    public void setContent(String content)
+    {
+        Content = content;
+    }
+    
+}
