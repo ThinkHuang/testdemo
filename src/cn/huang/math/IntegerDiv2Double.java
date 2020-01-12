00aa5867a10c72327ab872b636136574d162ea45
ThinkHuang <1070014845qq.com>
2020-01-12 11:42:45 +0800

    验证整数乘除法只会得到整数型值，而不会得到小数值

---
+++ src/cn/huang/math/IntegerDiv2Double.java
@@ -0,0 +1,11 @@
+package math;
+
+public class IntegerDiv2Double
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println(13 / 4);
+        System.out.println(Math.floor(13 / 4));
+    }
+}
